17add9248ca5c916a2187a1c7da760994b05640a
fixable11 <fixable11@gmail.com>
2018-10-24 11:53:38 +0300

    Added channel for threads

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -13,8 +13,18 @@ class RepliesController extends Controller
         $this->middleware('auth');
     }
     
-    public function store(Thread $thread)
+    /**
+     * Undocumented function
+     *
+     * @param [type] $channelId
+     * @param \App\Thread $thread
+     * @return void
+     */
+    public function store($channelId, Thread $thread)
     {
+        $this->validate(request(), [
+            'body' => 'required',
+        ]);
         $thread->addReply([
             'body' => request('body'),
             'user_id' => auth()->id()
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/threads', 'ThreadsController@index');
-Route::get('/threads/{thread}', 'ThreadsController@show');
 Route::get('/test',function(){
     phpinfo();
 });
@@ -24,4 +22,9 @@ Route::get('/test',function(){
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('threads', 'ThreadsController@index');
+Route::get('/threads/create', 'ThreadsController@create');
+Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
+Route::post('/threads', 'ThreadsController@store');
 Route::post('/threads/{thread}/replies', 'RepliesController@store')->name('add_reply_to_t');
+Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
