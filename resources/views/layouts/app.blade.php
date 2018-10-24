9b5a1f9929579333754194a3926155ac7860b3e1
fixable11 <fixable11@gmail.com>
2018-10-24 15:19:07 +0300

    Added validation errors and view composer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,13 +44,16 @@
                                     <li class="nav-item">
                                         <a class="nav-link" href="/threads">All threads</a>
                                     </li>
+                                    <li class="nav-item">
+                                            <a class="nav-link" href="/threads/create">Create Thread</a>
+                                    </li>
                                     <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                             Channels
                                         </a>
                                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                            @foreach (App\Channel::all() as $channel)
+                                            @foreach ($channels as $channel)
                                             <a class="dropdown-item" href="/threads/{{ $channel->slug }}">{{$channel->name
                                                 }}</a>
                                             @endforeach
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -10,18 +10,47 @@
                 <div class="card-body">
                     <form method="POST" action="/threads">
                         @csrf
+
+                        <div class="form-group">
+                            <label for="channel_id">Choose a channel</label>
+                            <select name="channel_id" id="channel_id" class="form-control" required>
+                                <option value="">Choose one..</option>
+
+                                @foreach ($channels as $channel)
+                                <option value="{{ $channel->id }}"
+                                    {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                    {{ $channel->name }}
+                                </option>
+                                @endforeach
+                            </select>
+                            <input type="hidden" class="form-control">
+                        </div>
+
                         <div class="form-group">
                             <label for="title">Title:</label>
-                            <input name="title" type="text" id="title" class="form-control" placeholder="title">
+                            <input required value="{{ old('title') }}" name="title" type="text" id="title" class="form-control"
+                                placeholder="title">
                         </div>
 
                         <div class="form-group">
                             <label for="body">Body:</label>
-                            <textarea name="body" type="text" id="body" class="form-control" placeholder="body"></textarea>
+                            <textarea required name="body" type="text" id="body" class="form-control" placeholder="body">{{ old('body') }}</textarea>
                         </div>
 
-                        <button type="submit" class="btn btn-primary">Publish</button>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Publish</button>
+                        </div>
+
+                        @if ($errors->any())
+                        <ul class="alert alert-danger">
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                        @endif
                     </form>
+
+
                 </div>
             </div>
         </div>
