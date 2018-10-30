0fcf1061fb676982bb4d93f5828b62453a2bd979
fixable11 <fixable11@gmail.com>
2018-10-30 21:36:52 +0200

    Clean up thread

--- app/Thread.php
+++ app/Thread.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\RecordsActivity;
 use App\Notifications\ThreadWasUpdated;
+use App\Events\ThreadHasNewReply;
 
 class Thread extends Model
 {
@@ -49,17 +50,19 @@ class Thread extends Model
     {
         $reply = $this->replies()->create($reply);
 
-        $this->subscriptions
-
-        ->filter(function($sub) use ($reply){
-            return $sub->user_id != $reply->user_id;
-        })
-
-        ->each(function ($sub) use ($reply){
-            $sub->notify($reply);
-        });
+        $this->notifySubscribers($reply);
 
         return $reply;
+    }
+
+    public function notifySubscribers($reply)
+    {
+
+        $this->subscriptions
+            ->where('user_id', '!=', $reply->user_id)
+            ->each(function ($sub) use ($reply){
+                $sub->notify($reply);
+            }); 
     }
 
     public function channel()
