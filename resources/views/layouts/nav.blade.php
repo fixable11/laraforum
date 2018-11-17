5238cc6bc36eecbcf3293308edd8852f267e5a4b
fixable11 <fixable11@gmail.com>
2018-11-17 15:04:14 +0200

    fixing layout

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -48,10 +48,10 @@
             <ul class="navbar-nav ml-auto">
                 <!-- Authentication Links -->
                 @guest
-                    <li class="nav-item">
+                    <li class="nav-item nav-link">
                         <a class="link" href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item nav-link">
                         @if (Route::has('register'))
                         <a class="link" href="{{ route('register') }}">{{ __('Register') }}</a>
                         @endif
